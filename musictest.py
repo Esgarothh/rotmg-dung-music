810ca48cd001358b5049dc20777299ae3cd12a8c
Esgarothh <83790801+Esgarothh@users.noreply.github.com>
2021-05-06 18:09:39 -0400

    Create musictest.py

---
+++ musictest.py
@@ -0,0 +1,11 @@
+import requests
+
+#Webhook of my channel. Click on edit channel --> Webhooks --> Creates webhook
+mUrl = "https://discord.com/api/webhooks/729017161942******/-CC0BNUXXyrSLF1UxjHMwuHA141wG-FjyOSDq2Lgt*******************"
+
+data = {"content": 'abc'}
+response = requests.post(mUrl, json=data)
+
+print(response.status_code)
+
+print(response.content)
