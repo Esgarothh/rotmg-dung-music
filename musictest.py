2e96f2742f0dc3511ae33efed5ae42be63815907
Esgarothh <83790801+Esgarothh@users.noreply.github.com>
2021-05-06 18:19:28 -0400

    Update musictest.py

--- musictest.py
+++ musictest.py
@@ -1,11 +1,5 @@
 import requests
+from discord import Webhook, RequestsWebhookAdapter
 
-#Webhook of my channel. Click on edit channel --> Webhooks --> Creates webhook   sss
-mUrl = "https://discord.com/api/webhooks/839985914070433803/RUcfOIxYWSvwIVPEn09t-DcF4jcEWRofJg_NVdFue4zy3_4kvk0nvEgkCU2YZTn_vo0f"
-
-data = {"content": 'abc'}
-response = requests.post(mUrl, json=data)
-
-print(response.status_code)
-
-print(response.content)
+webhook = Webhook.from_url("url-here", adapter=RequestsWebhookAdapter())
+webhook.send("Hello World")
